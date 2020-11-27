f64a403f788e64e1652a3a571c016adfec046477
prada2 <74619302+prada2@users.noreply.github.com>
2020-11-27 13:50:08 -0500

    Create coding_practice_2020_11_27.py

---
+++ coding_practice_2020_11_27.py
@@ -0,0 +1,69 @@
+## from 433 to 613 by 12
+
+# i = 433
+# while i <= 613:
+#     print(i)
+#     i += 12
+
+## from 0 to 144 by 12
+
+# i = 0
+# while i <= 144:
+#     print(i)
+#     i += 12
+
+## from 100 to 500 by 10
+
+# i = 100
+# while i <= 500:
+#     print(i)
+#     i += 10
+
+## from -333 to -50 by 33
+
+# i = -333
+# while i <= -50:
+#     print(i)
+#     i += 33
+
+## from 100 to 613 by 222
+
+# i = 100
+# while i <= 222:
+#     print(i)
+#     i += 11
+
+## from 400 to 500 by 20
+
+# i = 400
+# while i <= 500:
+#     print(i)
+#     i += 20
+
+## from 444 to 4444 by 100
+
+# i = 444
+# while i <= 4444:
+#     print(i)
+#     i += 100
+
+## from 505 to 555 by 5
+
+# i = 505
+# while i <= 555:
+#     print(i)
+#     i += 5
+
+## from 444 to 555 by 11
+
+# i = 444
+# while i <= 555:
+#     print(i)
+#     i += 11
+
+## from 757 to 1000 by 22
+
+# i = 757
+# while i <= 1000:
+#     print(i)
+#     i += 22
